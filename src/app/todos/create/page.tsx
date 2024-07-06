f3e873ab76396ec3f7a722ee385202607f951fd8
安部大貴 <76642241+NTD1995@users.noreply.github.com>
2024-07-07 01:31:25 +0900

    todo-app修正

--- src/app/todos/create/page.tsx
+++ src/app/todos/create/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>一覧画面</div>
+      <div className={styles.description}>作成画面</div>
     </main>
   );
 }
--- src/app/todos/page.create.tsx
+++
@@ -1,6 +0,0 @@
-import styles from './page.create.module.css';
-
-export default function Todos() {
-  return <div className={styles.main}>CREATE</div>;
-}
-
