4922e95ac156391c2ac5ef5046c6d9f9c718d7c2
安部大貴 <76642241+NTD1995@users.noreply.github.com>
2024-07-18 01:59:06 +0900

    todo-app修正

--- src/app/todos/create/page.tsx
+++ src/app/todos/create/page.tsx
@@ -1,9 +1,47 @@
-import styles from './page.module.css';
+// todoの作成画面
 
-export default function Home() {
+
+export default function Create() {
+  
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>作成画面</div>
-    </main>
+    <>
+      <h1>NEW TODO</h1>
+      <li style={{ listStyle: 'none' }}>
+            <div style={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
+              <p style={{ width: '50%', textAlign: 'start' }}></p>
+              <div style={{ width: '25%'}}>
+                <button>戻る</button>
+              </div>
+            </div>
+        </li>
+      <div style={{ padding: '16px', textAlign: 'center' }}>
+        <div style={{ display: 'flex', flexDirection: 'column',alignItems: 'center', gap: '16px' }}>
+          <div style={{ width: '100%',  }}>
+            <p>Title</p>
+             <textarea
+          style={{ width: '40em', border: '1px solid #333' }}
+        />
+          </div>
+         <div style={{ width: '100%' }}>
+            <p>Detail</p>
+            <textarea
+          style={{ width: '40em', border: '1px solid #333' }}
+        />
+          </div>
+          <div style={{ width: '100%' }}>
+            <p>Status</p>
+              <button>未着手</button>
+               <button>作業中</button>
+                <button>完了</button>
+          </div>
+        </div>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
+              <p style={{ width: '50%', textAlign: 'start' }}></p>
+              <div style={{ width: '25%'}}>
+                <button>作成</button>
+              </div>
+            </div>
+    </>
   );
 }
