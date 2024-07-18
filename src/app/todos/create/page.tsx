b0dacdfba10196aed2a1726b648f89f9f726b495
安部大貴 <76642241+NTD1995@users.noreply.github.com>
2024-07-19 01:31:45 +0900

    todo-aoo修正

--- src/app/todos/create/page.tsx
+++ src/app/todos/create/page.tsx
@@ -16,32 +16,41 @@ export default function Create() {
         </li>
       <div style={{ padding: '16px', textAlign: 'center' }}>
         <div style={{ display: 'flex', flexDirection: 'column',alignItems: 'center', gap: '16px' }}>
-          <div style={{ width: '100%',  }}>
+             <div style={{ width: '100%',  }}>
             <p>Title</p>
              <textarea
-          style={{ width: '40em', border: '1px solid #333' }}
-        />
-          </div>
-         <div style={{ width: '100%' }}>
+              style={{ width: '40em', border: '1px solid #333' }}
+              />
+                   </div>
+             <div style={{ width: '100%' }}>
             <p>Detail</p>
             <textarea
           style={{ width: '40em', border: '1px solid #333' }}
-        />
-          </div>
-          <div style={{ width: '100%' }}>
+                />
+                    </div>
             <p>Status</p>
-              <button>未着手</button>
-               <button>作業中</button>
-                <button>完了</button>
-          </div>
-        </div>
-      </div>
+            <div style={{ display: 'flex', gap: '10px' }}>
+              <label>
+                <input type="radio" name="status" value="未着手" />
+                未着手
+              </label>
+              <label>
+                <input type="radio" name="status" value="作業中" />
+                作業中
+              </label>
+              <label>
+                <input type="radio" name="status" value="完了" />
+                完了
+              </label>
+            </div>
       <div style={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
               <p style={{ width: '50%', textAlign: 'start' }}></p>
               <div style={{ width: '25%'}}>
                 <button>作成</button>
               </div>
             </div>
+        </div>
+       </div>   
     </>
   );
 }
