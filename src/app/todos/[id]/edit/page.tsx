93e2b0b86f0c2d687b41c31db74c1ec456e54dae
安部大貴 <76642241+NTD1995@users.noreply.github.com>
2024-07-21 00:05:27 +0900

    todo-app修正

--- src/app/todos/[id]/edit/page.tsx
+++ src/app/todos/[id]/edit/page.tsx
@@ -1,9 +1,30 @@
-import styles from './page.module.css';
+// todoの編集画面
 
-export default function Home() {
+export default function Edit() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>編集画面</div>
-    </main>
+    <>
+      <h1>EDIT TODO</h1>
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px' }}>
+      <button style={{ marginLeft: 'auto' }}>戻る</button>
+    </div>
+
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', gap: '20px', padding: '20px' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '100%' }}>
+        <p>Title</p>
+        <input style={{ width: '100%' }} />
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '100%' }}>
+        <p>Detail</p>
+        <textarea style={{ width: '100%', height: '100px' }}></textarea>
+      </div>
+     </div>
+
+            
+           <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px' }}>
+      <button style={{ marginLeft: 'auto' }}>更新 </button>
+    </div>
+        
+         
+    </>
   );
 }
