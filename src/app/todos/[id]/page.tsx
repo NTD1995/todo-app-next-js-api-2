78e8d0be508b0bd278b26d122cbc2a4937c61b16
安部大貴 <76642241+NTD1995@users.noreply.github.com>
2024-07-20 17:22:59 +0900

    todo-app修正

--- src/app/todos/[id]/page.tsx
+++ src/app/todos/[id]/page.tsx
@@ -1,9 +1,28 @@
-import styles from './page.module.css';
+// todoの詳細画面
 
-export default function Home() {
+export default function Detail() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>詳細画面</div>
-    </main>
+    <>
+      <h1>SHOW TODO</h1>
+       <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px' }}>
+      <button style={{ marginLeft: 'auto' }}>戻る</button>
+    </div>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', gap: '20px', padding: '20px' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '100%' }}>
+        <p>Title</p>
+        <input style={{ width: '100%' }} />
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '100%' }}>
+        <p>Detail</p>
+        <textarea style={{ width: '100%', height: '100px' }}></textarea>
+      </div>
+    </div>
+         <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px' }}>
+      <div style={{ flex: 1, display: 'flex', justifyContent: 'center', gap: '10px' }}>
+        <button>削除</button>
+        <button>編集</button>
+      </div>
+      </div>
+    </>
   );
 }
--- src/app/todos/create/page.tsx
+++ src/app/todos/create/page.tsx
@@ -4,30 +4,21 @@ export default function Create() {
   return (
     <>
       <h1>NEW TODO</h1>
-      <li style={{ listStyle: 'none' }}>
-        <div style={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
-          <p style={{ width: '50%', textAlign: 'start' }}></p>
-          <div style={{ width: '25%' }}>
-            <button>戻る</button>
-          </div>
-        </div>
-      </li>
-      <div style={{ padding: '16px', textAlign: 'center' }}>
-<<<<<<< HEAD
-        <div style={{ display: 'flex', flexDirection: 'column',alignItems: 'center', gap: '16px' }}>
-             <div style={{ width: '100%',  }}>
-            <p>Title</p>
-             <textarea
-              style={{ width: '40em', border: '1px solid #333' }}
-              />
-                   </div>
-             <div style={{ width: '100%' }}>
-            <p>Detail</p>
-            <textarea
-          style={{ width: '40em', border: '1px solid #333' }}
-                />
-                    </div>
-            <p>Status</p>
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px' }}>
+      <button style={{ marginLeft: 'auto' }}>戻る</button>
+    </div>
+
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', gap: '20px', padding: '20px' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '100%' }}>
+        <p>Title</p>
+        <input style={{ width: '100%' }} />
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '100%' }}>
+        <p>Detail</p>
+        <textarea style={{ width: '100%', height: '100px' }}></textarea>
+      </div>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '100%' }}></div>
+           <p>Status</p>
             <div style={{ display: 'flex', gap: '10px' }}>
               <label>
                 <input type="radio" name="status" value="未着手" />
@@ -42,41 +33,14 @@ export default function Create() {
                 完了
               </label>
             </div>
-      <div style={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
-              <p style={{ width: '50%', textAlign: 'start' }}></p>
-              <div style={{ width: '25%'}}>
-                <button>作成</button>
-              </div>
-            </div>
-        </div>
-       </div>   
-=======
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '16px' }}>
-          <div style={{ width: '100%' }}>
-            <p>Title</p>
-            <textarea style={{ width: '40em', border: '1px solid #333' }} />
-          </div>
-          <div style={{ width: '100%' }}>
-            <p>Detail</p>
-            <textarea style={{ width: '40em', border: '1px solid #333' }} />
-          </div>
-          <div style={{ width: '100%' }}>
-            <p>Status</p>
-            {/* TODO: ここはbuttonではなく、inputを使ってラジオボタンで実装しましょう！ */}
-            {/* 参考：https://zero-plus.io/media/how-to-implement-a-radio-button/ */}
-            <button>未着手</button>
-            <button>作業中</button>
-            <button>完了</button>
-          </div>
-        </div>
       </div>
-      <div style={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
-        <p style={{ width: '50%', textAlign: 'start' }}></p>
-        <div style={{ width: '25%' }}>
-          <button>作成</button>
-        </div>
-      </div>
->>>>>>> 61c95733a1e65c20486a058cb7ff7ec6ddca2e3c
+
+            
+           <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px' }}>
+      <button style={{ marginLeft: 'auto' }}>作成</button>
+    </div>
+        
+         
     </>
   );
 }
