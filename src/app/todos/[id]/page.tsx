7d447a7c96b06926cb3b8d16fbf397c92dde3f41
Shogo Wada <aiz4338150@gmail.com>
2024-07-28 13:12:38 +0900

    詳細画面のレビュー②

--- src/app/todos/[id]/page.tsx
+++ src/app/todos/[id]/page.tsx
@@ -6,24 +6,26 @@ export default function Detail() {
       <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px' }}>
         <button style={{ marginLeft: 'auto' }}>戻る</button>
       </div>
-      <div style={{ width: '100%' ,border: '1px solid black', padding: '30px', margin: '10px' }}>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', gap: '20px', padding: '20px' }}>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '100%' }}>
-          <p>Title</p>
-          <p style={{ width: '100%' }}>GITHUB上に静的サイトをホスティングする</p>
-        </div>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '100%' }}>
-          <p>Detail</p>
-          <p style={{ width: '100%', height: '100px' }}>
-            AWSコンソールでAWS AMPLIFYを使って静的ウェブサイトをホスティングします。AWS
-            AMPLIFYは、静的ウェブサイトおよびウェブアプリにフルマネージドのホスティングを提供します。AMPLIFYのホスティングソリューションは、AMAZON
-            CLOUDFRONTとAMAZON S3を使って、AWSコンテンツ配信ネットワーク(CDN)を介してサイトアセットを提供します。
-            <br />
-            継続的デプロイをセットアップします。AMPLIFYは、継続的デプロイでGITベースのワークフローを提供します。それにより、コードコミットごとに、サイトに自動的に更新をデプロイすることができます。
-          </p>
+      <div style={{ width: '100%', padding: '30px', margin: '10px' }}>
+        <div
+          style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', gap: '20px', padding: '20px' }}
+        >
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '100%' }}>
+            <p style={{ width: '100%', border: '1px solid black' }}>Title</p>
+            <p style={{ width: '100%' }}>GITHUB上に静的サイトをホスティングする</p>
+          </div>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '100%' }}>
+            <p style={{ width: '100%', border: '1px solid black' }}>Detail</p>
+            <p style={{ width: '100%' }}>
+              AWSコンソールでAWS AMPLIFYを使って静的ウェブサイトをホスティングします。AWS
+              AMPLIFYは、静的ウェブサイトおよびウェブアプリにフルマネージドのホスティングを提供します。AMPLIFYのホスティングソリューションは、AMAZON
+              CLOUDFRONTとAMAZON S3を使って、AWSコンテンツ配信ネットワーク(CDN)を介してサイトアセットを提供します。
+              <br />
+              継続的デプロイをセットアップします。AMPLIFYは、継続的デプロイでGITベースのワークフローを提供します。それにより、コードコミットごとに、サイトに自動的に更新をデプロイすることができます。
+            </p>
+          </div>
         </div>
       </div>
-    </div>
       <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px' }}>
         <div style={{ flex: 1, display: 'flex', justifyContent: 'center', gap: '10px' }}>
           <button>削除</button>
